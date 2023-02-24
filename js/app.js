eea2eddc36f09342847f5aef2d47dc89ba3552c6
Muazkhalid786 <itzmajo786@gmail.com>
2023-02-24 15:31:27 +0500

    Upload the project files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+
+var typed = new Typed('#course', {
+    strings: ['C++', 'Python <img class="langImg" src="../img/python.png" >',  "C#", "JavaScript", "TypeScript", "Swift", "Microsoft Powerpoint", "Microsoft Word"],
+    smartBackspace: true, // Default value
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+  });
