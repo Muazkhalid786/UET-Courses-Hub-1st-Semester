99725eaf597a1a639c5344988d660692528db69c
Muazkhalid786 <itzmajo786@gmail.com>
2023-03-03 10:17:13 +0500

    Add all pages

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log("Hello")
 
 var typed = new Typed('#course', {
-    strings: ['C++', 'Python <img class="langImg" src="../img/python.png" >',  "C#", "JavaScript", "TypeScript", "Swift", "Microsoft Powerpoint", "Microsoft Word"],
+    strings: ['C++', 'Python <img class="langImg"  src="../img/python.png" >',  "C#", "JavaScript", "TypeScript", "Swift", "Microsoft Powerpoint", "Microsoft Word"],
     smartBackspace: true, // Default value
     typeSpeed: 150,
     backSpeed: 100,
